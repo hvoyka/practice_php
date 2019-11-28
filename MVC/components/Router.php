dd716c1d9b65ce78afd5addbb09bc00af20ca595
hvoyka <hvoinii@gmail.com>
2019-11-28 18:44:03 +0600

    create function getUri() in Router.php

--- MVC/components/Router.php
+++ MVC/components/Router.php
@@ -9,10 +9,24 @@ class Router
         $routesPath = ROOT . '/config/routes.php';
         $this->routes = include ($routesPath);
     }
+    /**
+     * Returns request string
+     * @return string
+     */
+    private function getURI()
+    {
+        if(!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/MVC'); //folder MVC in my project
+        }
+    }
+
+
     public function run()
     {
         //Получить строку запроса
-        
+        $uri = $this->getURI();
+        echo $uri;
+        //echo $_SERVER['REQUEST_URI'];
         //Проверить наличие такого запроса в routes.php
         
         // Если есть совпадения, определить какой контроллер 

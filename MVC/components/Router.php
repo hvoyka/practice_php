ef73efeb52261d214d40caa3c67982fd32423576
hvoyka <hvoinii@gmail.com>
2019-11-28 18:33:04 +0600

    prepare to write run() function

--- MVC/components/Router.php
+++ MVC/components/Router.php
@@ -6,10 +6,20 @@ class Router
     
     public function __construct()
     {
-        
+        $routesPath = ROOT . '/config/routes.php';
+        $this->routes = include ($routesPath);
     }
     public function run()
     {
-        echo 'Class Router, method run';
+        //Получить строку запроса
+        
+        //Проверить наличие такого запроса в routes.php
+        
+        // Если есть совпадения, определить какой контроллер 
+        // и action обрабатывает запрос
+        
+        // Подключить фаил класса-контроллера
+        
+        // Создать объект, вызвать метод (т.е. action)
     }
 }
--- MVC/index.php
+++ MVC/index.php
@@ -8,7 +8,8 @@ error_reporting(E_ALL);
 // 2. Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 require_once (ROOT . '/components/Router.php');
-echo ROOT;
 // 3. Установка соединенния с БД
 
 // 4. Вызов Router
+$router = new Router();
+$router->run();

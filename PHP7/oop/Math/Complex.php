9c0c526bc5e9fc97878e9e6b9ff5fa695c5554ea
Lavrentev Stanislav <hvoinii@gmail.com>
2020-10-06 19:30:07 +0600

    php7 book p 425-427, oop create new class

---
+++ PHP7/oop/Math/Complex.php
@@ -0,0 +1,11 @@
+<?php
+class MathComplex
+{
+  public $re, $im;
+  function add ($re, $im)
+  {
+    $this->re += $re;
+    $this->im += $im;
+  }
+}
+?>
---
+++ PHP7/oop/call.php
@@ -0,0 +1,12 @@
+<?php
+  require_once "Math/Complex.php";
+
+  $obj = new MathComplex;
+
+  $obj->re = 16.7;
+  $obj->im = 101;
+
+  $obj->add(18.09, 303);
+
+  echo "({$obj->re}, {$obj->im})";
+?>

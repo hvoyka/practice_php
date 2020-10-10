81498f65dd376d4aca16541443f4b660238b2c46
Lavrentev Stanislav <hvoinii@gmail.com>
2020-10-10 20:59:02 +0600

    php7 book p 444, static, self

---
+++ PHP7/oop/static.php
@@ -0,0 +1,24 @@
+<?php
+  class Counter
+  {
+    private static $count = 0;
+    public function __construct()
+    {
+      self::$count++;
+    }
+    public function __destruct()
+    {
+      self::$count--;
+    }
+    public static function getCount() {return self::$count;}
+  }
+
+  for ($objs = [], $i = 0; $i< 6; $i++)
+    $objs[] = new Counter();
+
+  echo "Сейчас существует {$objs[0]->getCount()} объектов. <br>";
+  
+  $objs[5] = null;
+
+  echo "Под конец осталось - " . Counter::getCount() . " объектов. <br>"
+?>
